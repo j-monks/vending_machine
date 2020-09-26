9f9cab0af04833a2dfa4678bd1bb3dc88e0b9549
James Monks <J.monks23@gmail.com>
2020-09-26 15:02:18 +0100

    basic vending machine stuff set up

--- src/main/java/machine/vendingMachine.java
+++ src/main/java/machine/vendingMachine.java
@@ -1,4 +1,32 @@
 package machine;
 
+import products.ProductType;
+
+import java.util.ArrayList;
+
 public class vendingMachine {
+
+    private double moneyBox;
+    private ArrayList<ProductType> inventory;
+
+    public vendingMachine(double moneyBox) {
+        this.moneyBox = 0.00;
+        this.inventory = new ArrayList<ProductType>();
+    }
+
+    public double getMoneyBox() {
+        return moneyBox;
+    }
+
+    public ArrayList<ProductType> getInventory() {
+        return inventory;
+    }
+
+    public void setMoneyBox(double moneyBox) {
+        this.moneyBox = moneyBox;
+    }
+
+    public void setInventory(ArrayList<ProductType> inventory) {
+        this.inventory = inventory;
+    }
 }
